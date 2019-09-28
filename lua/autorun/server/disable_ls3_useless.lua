5507a88c6cd6826aaf28ac68f0f5120341794130
Mark Dietzer <git@doridian.net>
2019-09-27 17:43:03 -0700

    rm ls3 oxygen

--- lua/autorun/server/disable_ls3_useless.lua
+++ lua/autorun/server/disable_ls3_useless.lua
@@ -1,16 +1,11 @@
 timer.Simple(1, function()
 	local RD = CAF.GetAddon("Resource Distribution")
-	if not SA_OLD_REG then
-	                SA_OLD_REG = RD.RegisterNonStorageDevice
-	end
-	if not SA_OLD_REG then
-	        error("WAT")
-	end
+	local SA_OLD_REG = RD.RegisterNonStorageDevice
 	function RD.RegisterNonStorageDevice(ent)
-	        if ent.caf.custom.resource == "oxygen" then
-	                ent:Remove()
-	                return
-	        end
-	        SA_OLD_REG(ent)
+		if ent.caf.custom.resource == "oxygen" then
+			ent:Remove()
+			return
+		end
+		SA_OLD_REG(ent)
 	end
 end)
