aaecbde7c3105b2413abc6b0026262a39d5b137e
Mark Dietzer <git@doridian.net>
2019-09-29 00:03:35 -0700

    rm this

--- lua/autorun/server/sv_sa_entity_blacklist.lua
+++ lua/autorun/server/sv_sa_entity_blacklist.lua
@@ -1,15 +1,3 @@
-timer.Simple(1, function()
-	local RD = CAF.GetAddon("Resource Distribution")
-	local SA_OLD_REG = RD.RegisterNonStorageDevice
-	function RD.RegisterNonStorageDevice(ent)
-		if ent and ent.caf and ent.caf.custom and ent.caf.custom.resource == "oxygen" then
-			ent:Remove()
-			return
-		end
-		SA_OLD_REG(ent)
-	end
-end)
-
 local BadSENTs = {"sent_ball"}
 
 hook.Add("PlayerSpawnSENT", "SA_StopBadProps", function(ply, ent)
