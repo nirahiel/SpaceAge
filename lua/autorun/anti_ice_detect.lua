f3754be5848e870755b6a21d5ef477b527e87561
Mark Dietzer <git@doridian.net>
2019-10-06 21:02:40 -0700

    Improve anti-ice-detect, even serverside

--- lua/autorun/anti_ice_detect.lua
+++ lua/autorun/anti_ice_detect.lua
@@ -1,14 +1,46 @@
+local SERVER = SERVER
+local dgetinfo = debug.getinfo
+local pairs = pairs
+local GetEntityClass = FindMetaTable("Entity").GetClass
+
+local CALLED_IDX = 2
+local CALLER_IDX = 3
+local OKAY_CALLERS = { "addons/spaceage/", "lua/fpp/", "lua/ulib/" }
+
+SA.RunAntiIceDebug = false
+
 if SERVER then
 	AddCSLuaFile()
-	return
 end
 
 local function IsOkay(ent)
-	if SA.ValidEntity(ent) and ent:GetClass() == "iceroid" then
+	if SA.ValidEntity(ent) and GetEntityClass(ent) == "iceroid" then
 		return false
 	end
 
 	return true
+end
+
+
+local function SkipFilter()
+	if not SERVER then
+		return false
+	end
+
+	local caller = dgetinfo(CALLER_IDX, "S")
+
+	for _, v in pairs(OKAY_CALLERS) do
+		if caller.short_src:find(v, 1, true) == 1 then
+			return true
+		end
+	end
+
+	if SA.RunAntiIceDebug then
+		local funcCalled = dgetinfo(CALLED_IDX).name
+		file.Append("findlog.txt", funcCalled .. "\n" .. util.TableToJSON(caller) .. "\n\n")
+	end
+
+	return false
 end
 
 local function FilterTable(tbl)
@@ -28,7 +60,11 @@ local function OverwriteTableFunc(idx, tbl)
 
 	local old = tbl[idx]
 	tbl[idx] = function(...)
-		return FilterTable(old(...))
+		local data = old(...)
+		if SkipFilter() then
+			return data
+		end
+		return FilterTable(data)
 	end
 end
 
@@ -38,6 +74,10 @@ local function OverwriteSingleFunc(idx, tbl)
 	local old = tbl[idx]
 	tbl[idx] = function(...)
 		local ent = old(...)
+		if SkipFilter() then
+			return ent
+		end
+
 		if IsOkay(ent) then
 			return ent
 		end
@@ -45,8 +85,28 @@ local function OverwriteSingleFunc(idx, tbl)
 	end
 end
 
+local ICEROID = "iceroid"
+local function IsIceroidWildcard(cls)
+	if cls == ICEROID then
+		return true
+	end
+
+	return cls:find("[^A-Za-z0-9_]")
+end
+
+local oldFindByClass = ents.FindByClass
+function ents.FindByClass(cls)
+	local data = oldFindByClass(cls)
+	if not IsIceroidWildcard(cls) then
+		return data
+	end
+	if SkipFilter() then
+		return data
+	end
+	return FilterTable(data)
+end
+
 OverwriteTableFunc("FindByModel")
-OverwriteTableFunc("FindByClass")
 OverwriteTableFunc("FindInBox")
 OverwriteTableFunc("FindInCone")
 OverwriteTableFunc("FindInSphere")
