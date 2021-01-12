8fecb824b23fed9a56aa23fa232b146b35de3f2e
Mark Dietzer <git@doridian.net>
2021-01-11 19:07:09 -0800

    debug

--- lua/autorun/sa_functions.lua
+++ lua/autorun/sa_functions.lua
@@ -51,8 +51,8 @@ function SA.ValidEntity(ent)
 	return ent and ent:IsValid()
 end
 
-local MAP_MIN = -32767
-local MAX_MAX = 32767
+local MAP_MIN = -16384
+local MAX_MAX = 16384
 function SA.IsInsideMap(vector)
 	local ok = vector.x >= MAP_MIN and vector.y >= MAP_MIN and vector.z >= MAP_MIN and vector.x <= MAX_MAX and vector.y <= MAX_MAX and vector.z <= MAX_MAX
 	print(vector, ok)
