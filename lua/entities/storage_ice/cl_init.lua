7c091c73fb67404e296bb174087dc6c7ea3de0c5
Mark Dietzer <git@doridian.net>
2021-01-17 15:02:28 -0800

    rm these

--- lua/entities/storage_ice/cl_init.lua
+++ lua/entities/storage_ice/cl_init.lua
@@ -1,6 +1,2 @@
 include("shared.lua")
 language.Add("storage_ice", "Ice Storage Module")
-
-function ENT:Draw()
-	self:DrawModel()
-end
--- lua/entities/storage_ice_product/cl_init.lua
+++ lua/entities/storage_ice_product/cl_init.lua
@@ -1,7 +1,3 @@
 include("shared.lua")
 
 language.Add("storage_ice_product", "Ice Product Storage Module")
-
-function ENT:Draw()
-	self:DrawModel()
-end
