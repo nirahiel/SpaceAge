370acc14fd472ac12da340388bd2d99c030387dc
Mark Dietzer <git@doridian.net>
2021-01-20 13:14:28 -0800

    Limit TTS

--- lua/entities/gmod_wire_expression2/core/custom/sa_tts.lua
+++ lua/entities/gmod_wire_expression2/core/custom/sa_tts.lua
@@ -1,8 +1,15 @@
 --glualint:ignore-file
 E2Lib.RegisterExtension("sa_tts", false)
 
+local TTS_MAX_LENGTH = 256
+
 __e2setcost(50)
 e2function void playTTS(string text)
+	if text:len() > TTS_MAX_LENGTH then
+		error("TTS is limited to 256 characters!", 2)
+		return
+	end
+
 	http.Post("https://api.spaceage.mp/tts/mp3", { q = text }, function (body, length, headers, code)
 		if code ~= 200 then
 			return
