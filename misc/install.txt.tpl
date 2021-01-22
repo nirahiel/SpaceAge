dfb287844499fd6c40f68db644ac81d107412f4d
Mark Dietzer <git@doridian.net>
2021-01-22 12:15:24 -0800

    Add Install txt template and x64 srcds

--- misc/install.txt.tpl
+++ misc/install.txt.tpl
@@ -1,6 +1,6 @@
 @ShutdownOnFailedCommand 1
 @NoPromptForPassword 1
 login anonymous
-force_install_dir /home/server1/s/
+force_install_dir __HOME__/s/
 app_update 4020
 quit
--- misc/run.sh
+++ misc/run.sh
@@ -2,7 +2,7 @@
 # Main server runscript
 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
-export LD_LIBRARY_PATH=".:bin:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH=".:bin/linux64:$LD_LIBRARY_PATH"
 
 export WORKSHOP_COLLECTION=177294269
 
@@ -20,8 +20,11 @@ upgrade_addon sbep
 upgrade_addon smartsnap
 upgrade_addon wire-extras
 
+INSTALL_TXT="$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
+sed "s~__HOME__~$HOME~" "${INSTALL_TXT}.tpl" > "$INSTALL_TXT"
+
 steamcmd +runscript "$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
 
 "$HOME/s/garrysmod/addons/spaceage/misc/grabaddons.sh"
 
-exec ./srcds_linux -usercon -autoupdate -tickrate 20 -game garrysmod +ip 0.0.0.0 +maxplayers 32 +map sb_gooniverse_v4 +host_workshop_collection "$WORKSHOP_COLLECTION" +gamemode spaceage -disableluarefresh -console
+exec ./bin/linux64/srcds -usercon -autoupdate -tickrate 20 -game garrysmod +ip 0.0.0.0 +maxplayers 32 +map sb_gooniverse_v4 +host_workshop_collection "$WORKSHOP_COLLECTION" +gamemode spaceage -disableluarefresh -console
