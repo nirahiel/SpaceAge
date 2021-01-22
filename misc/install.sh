de4e434b1465f0cf2ec89a51a35302d7eee42508
Mark Dietzer <git@doridian.net>
2021-01-22 12:20:56 -0800

    Add easy install script

---
+++ misc/install.sh
@@ -0,0 +1,11 @@
+cd "$HOME/s"
+
+cd gamemodes
+ln -s ../addons/spaceage/gamemodes/spaceage ./
+ln -s ../addons/spacebuild/gamemodes/spacebuild ./
+cd ../lua
+ln -s ../addons/spaceage/misc/bin ./
+
+cd ../cfg
+cp ../addons/spaceage/misc/server.cfg server.cfg
+cp ../addons/spaceage/misc/game.cfg game.cfg
