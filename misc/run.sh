6047d1613ff0f5e0a5e27b91316c02666f2190a0
Mark Dietzer <git@doridian.net>
2021-01-23 11:47:12 -0800

    Split workshop collection

--- misc/run.sh
+++ misc/run.sh
@@ -4,8 +4,6 @@
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 export LD_LIBRARY_PATH="$HOME/s/bin/linux64"
 
-export WORKSHOP_COLLECTION=177294269
-
 cd "$HOME/s"
 
 upgrade_addon() {
@@ -24,6 +22,7 @@ INSTALL_TXT="$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
 sed "s~__HOME__~$HOME~" "${INSTALL_TXT}.tpl" > "$INSTALL_TXT"
 steamcmd +runscript "$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
 
+export WORKSHOP_COLLECTION=177294269
 "$HOME/s/garrysmod/addons/spaceage/misc/grabaddons.sh"
 
-exec ./bin/linux64/srcds -usercon -autoupdate -tickrate 20 -game garrysmod +ip 0.0.0.0 +maxplayers 32 +map sb_gooniverse_v4 +host_workshop_collection "$WORKSHOP_COLLECTION" +gamemode spaceage -disableluarefresh -console
+exec ./bin/linux64/srcds -usercon -autoupdate -tickrate 20 -game garrysmod +ip 0.0.0.0 +maxplayers 32 +map sb_gooniverse_v4 +host_workshop_collection "2371557248" +gamemode spaceage -disableluarefresh -console
