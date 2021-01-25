8bb4109e2ba22cbb6bfac69e32b6302f9fe6dd16
Mark Dietzer <git@doridian.net>
2021-01-25 01:05:37 -0800

    fix shit

--- lua/entities/sa_storage_tiberium_holder/shared.lua
+++ lua/entities/sa_storage_tiberium_holder/shared.lua
@@ -5,4 +5,4 @@ ENT.PrintName = "Tiberium Storage Holder"
 ENT.Spawnable = true
 ENT.AdminSpawnable = true
 
-list.Set("LSEntOverlayText" , "tiberium_storage_holder", {HasOOO = true, num = 1, resnames = {"tiberium"}})
+list.Set("LSEntOverlayText" , "sa_storage_tiberium_holder", {HasOOO = true, num = 1, resnames = {"tiberium"}})
