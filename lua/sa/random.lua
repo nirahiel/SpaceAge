bcd4efd1ff95e5accc16e59d490ee548ad698248
Mark Dietzer <git@doridian.net>
2021-01-28 20:49:01 -0800

    enhance

--- lua/sa/random.lua
+++ lua/sa/random.lua
@@ -1,9 +1,9 @@
 print("RandomLoad", pcall(require, "random"))
 
 local function ReseedRandom()
-	local seed = os.time() + math.random(0, 2147483647)
+	local seed = os.time() + math.random(-2147483648, 2147483647)
 	if SecureRandomNumber then
-		seed = seed + SecureRandomNumber(0, 2147483647)
+		seed = seed + SecureRandomNumber(-2147483648, 2147483647)
 	end
 	math.randomseed(seed)
 end
