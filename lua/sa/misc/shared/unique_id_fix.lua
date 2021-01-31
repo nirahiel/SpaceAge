66aadc51f6749f4c7cc5c085824407879af679dc
Mark Dietzer <git@doridian.net>
2021-01-30 20:27:14 -0800

    rm useless files

--- lua/sa/misc/shared/unique_id_fix.lua
+++
@@ -1,7 +0,0 @@
-local PlayerMeta = FindMetaTable("Player")
-function PlayerMeta:UniqueID2()
-    if not self._UniqueID then
-        self._UniqueID = util.CRC("gm_" .. self:SteamID() .. "_gm")
-    end
-    return self._UniqueID
-end
