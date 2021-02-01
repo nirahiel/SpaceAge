4b54a62e26bea0826db7b025637c01e333a5aba9
Mark Dietzer <git@doridian.net>
2021-01-31 22:40:20 -0800

    Add restart_if_empty

---
+++ lua/autorun/server/restartifempty.lua
@@ -0,0 +1,19 @@
+local convarEnabled = CreateConVar("restart_if_empty", 0)
+local convarMode = CreateConVar("restart_if_empty_mode", "exit")
+
+timer.Create("RestartIfEmpty", 1, 0, function()
+	if not convarEnabled:GetBool() then
+		return
+	end
+
+	if player.GetCount() > 0 then
+		return
+	end
+
+	local mode = convarMode:GetString()
+	if mode == "exit" then
+		RunConsoleCommand("exit")
+	elseif mode == "changelevel" then
+		RunConsoleCommand("changelevel", game.GetMap())
+	end
+end)
