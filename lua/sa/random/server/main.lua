970feda2311f650532f1fa55e65344981895902d
Mark Dietzer <git@doridian.net>
2021-02-07 12:29:01 -0800

    rm FFI

--- lua/sa/random/server/main.lua
+++ lua/sa/random/server/main.lua
@@ -1,47 +1,41 @@
-print("FFI Load", pcall(require, "ffi"))
+print("Random Load", pcall(require, "random"))
 
 SA.Random = {}
 
-local function mkAlphabet(chars)
-	local tbl = {}
-	local len = chars:len()
-	for i = 1, len do
-		table.insert(tbl, chars:sub(i, i))
-	end
-	return tbl, len
-end
-local alphabet, alphabetLen = mkAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
-
-local randomres, randomresSize
-if ffi then
-	ffi.cdef [[
-		struct RandomResult { int32_t value; };
-		size_t getrandom(void *buf, size_t buflen, unsigned int flags);
-	]]
-
-	if ffi.C.getrandom then
-		randomres = ffi.new("struct RandomResult")
-		randomresSize = ffi.sizeof(randomres)
-
-		function SA.Random.String(len)
-			local res = {}
-			local buf = ffi.new("char[" .. len .. "]")
-			ffi.C.getrandom(buf, len, 0)
-			for i = 1, len do
-				local x = (buf[i] % alphabetLen) + 1
-				table.insert(res, alphabet[x])
-			end
-			return table.concat(res)
-		end
-	end
+if SecureRandomString then
+	SA.Random.String = SecureRandomString
 end
 
 if not SA.Random.String then
-	function SA.Random.String(len)
+	local function mkAlphabet(chars)
+		local tbl = {}
+		local len = chars:len()
+		for i = 1, len do
+			table.insert(tbl, chars:sub(i, i))
+		end
+		return tbl, len
+	end
+	local alphabet, alphabetLen = mkAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/")
+
+	function SA.Random.String(len, allowAllOrAlphabet)
+		if allowAllOrAlphabet == true then
+			local res = {}
+			for i = 1, len do
+				local x = math.random(0, 255)
+				table.insert(res, string.char(x))
+			end
+			return table.concat(res)
+		end
+
+		local thisAlphabet, thisAlphabetLen = alphabet, alphabetLen
+		if allowAllOrAlphabet then
+			thisAlphabet, thisAlphabetLen = mkAlphabet(allowAllOrAlphabet)
+		end
+
 		local res = {}
 		for i = 1, len do
-			local x = math.random(1, alphabetLen)
-			table.insert(res, alphabet[x])
+			local x = math.random(1, thisAlphabetLen)
+			table.insert(res, thisAlphabet[x])
 		end
 		return table.concat(res)
 	end
@@ -49,9 +43,8 @@ end
 
 local function ReseedRandom()
 	local seed = os.time() + math.random(-2147483647, 2147483647)
-	if randomres then
-		ffi.C.getrandom(randomres, randomresSize, 0)
-		seed = seed + randomres.value
+	if SecureRandomNumber then
+		seed = seed + SecureRandomNumber(-2147483647, 2147483647)
 	end
 	math.randomseed(seed)
 end
