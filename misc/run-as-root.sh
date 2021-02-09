5b474c01b4857c5773e3f6a430dd3dd86be9a440
Mark Dietzer <git@doridian.net>
2021-02-09 14:07:21 -0800

    Update run-as-root.sh

--- misc/run-as-root.sh
+++ misc/run-as-root.sh
@@ -10,4 +10,4 @@ echo steam steamcmd/license note '' | debconf-set-selections
 dpkg --add-architecture i386
 apt update
 apt -y dist-upgrade
-apt -y install openssl git steamcmd tmux sudo curl htop haveged
+apt -y install openssl git steamcmd tmux sudo curl htop haveged libsdl2-2.0-0
