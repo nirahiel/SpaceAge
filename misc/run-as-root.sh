d4082a3cdd6454d1b6c8675128998fa017d027a4
Mark Dietzer <git@doridian.net>
2021-02-09 13:58:18 -0800

    Update run-as-root.sh

--- misc/run-as-root.sh
+++ misc/run-as-root.sh
@@ -2,6 +2,11 @@
 
 adduser server1
 
+export DEBIAN_FRONTEND=noninteractive
+
+echo steam steamcmd/question select "I AGREE" | sudo debconf-set-selections
+echo steam steamcmd/license note '' | sudo debconf-set-selections
+
 dpkg --add-architecture i386
 apt update
 apt -y dist-upgrade
