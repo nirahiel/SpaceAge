be2dd253900eed0b43aa923c68849096e2e820b9
Mark Dietzer <git@doridian.net>
2021-02-12 12:28:24 -0800

    Improve progression

--- lua/sa/research/shared/functions.lua
+++ lua/sa/research/shared/functions.lua
@@ -61,7 +61,7 @@ function SA.Research.GetLevelInfo(ply, Research, stopOnFail, level)
 		devl = 0
 	end
 	local total = cost + (cost * Research.costinc) * (level - 1)
-	total = total + math.ceil(total * (devl * 0.8))
+	total = mmath.ceil(total + (total * (devl * 0.8)))
 
 	if ply.sa_data.faction_name == "legion" or ply.sa_data.faction_name == "alliance" then
 		total = math.ceil(total * 0.66)
