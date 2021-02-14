094054e03398fef2164333431861c961ab719831
Mark Dietzer <git@doridian.net>
2021-02-13 23:59:07 -0800

    Allow triggering RIE remotely. Also force restart with AFK

--- lua/autorun/server/restart_if_empty.lua
+++ lua/autorun/server/restart_if_empty.lua
@@ -1,14 +1,17 @@
 local convarEnabled = CreateConVar("restart_if_empty", 0)
 local convarTime = CreateConVar("restart_if_empty_time", 60 * 10)
+local convarTimeAFK = CreateConVar("restart_if_empty_time_afk", 60 * 30)
 local convarMode = CreateConVar("restart_if_empty_mode", "exit")
 
 convarEnabled:SetBool(false)
 
-local function IsServerEmpty()
-	return player.GetCount() <= 0
-end
-
 local serverEmptyCycles = 0
+
+timer.Simple(0, function()
+	SA.Central.Handle("restart_if_empty", function()
+		convarEnabled:SetBool(true)
+	end)
+end)
 
 timer.Create("RestartIfEmpty", 1, 0, function()
 	if not convarEnabled:GetBool() then
@@ -16,15 +19,31 @@ timer.Create("RestartIfEmpty", 1, 0, function()
 		return
 	end
 
-	if not IsServerEmpty() then
+	local players = player.GetHumans()
+
+	local requiredCyclesCV = convarTime
+	for _, ply in pairs(players) do
+		requiredCyclesCV = convarTimeAFK
+		if not ply.IsAFK then
+			requiredCyclesCV = nil
+			break
+		end
+	end
+
+	if not requiredCyclesCV then
 		serverEmptyCycles = 0
 		return
 	end
 
+	local requiredCycles = requiredCyclesCV:GetInt()
+
 	serverEmptyCycles = serverEmptyCycles + 1
-	local requiredCycles = convarTime:GetInt()
 	if serverEmptyCycles <= requiredCycles then
-		print("Seconds until restart: ", requiredCycles - serverEmptyCycles)
+		local str = "Seconds until restart: " .. tostring(requiredCycles - serverEmptyCycles)
+		print(str)
+		for _, ply in pairs(players) do
+			ply:ChatPrint(str)
+		end
 		return
 	end
 
