f37456c48eb58450f0ad56b34afe5ac961e4908c
Mark Dietzer <git@doridian.net>
2021-02-25 23:43:48 -0800

    rm all the scripts that starlord now does

--- misc/install.txt.tpl
+++
@@ -1,6 +0,0 @@
-@ShutdownOnFailedCommand 1
-@NoPromptForPassword 1
-login anonymous
-force_install_dir __HOME__/s/
-app_update 4020 -beta x86-64
-quit
--- misc/steamcmd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-INSTALL_TXT="$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
-sed "s~__HOME__~$HOME~" "${INSTALL_TXT}.tpl" > "$INSTALL_TXT"
-steamcmd +runscript "$HOME/s/garrysmod/addons/spaceage/misc/install.txt"
