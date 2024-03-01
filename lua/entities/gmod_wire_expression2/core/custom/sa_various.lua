8dfd4f388a883cca89acb60acfc594d2946ea431
Doridian <git@doridian.net>
2024-02-29 20:35:29 -0800

    And playtime

--- lua/entities/gmod_wire_expression2/core/custom/sa_various.lua
+++ lua/entities/gmod_wire_expression2/core/custom/sa_various.lua
@@ -11,3 +11,13 @@ end
 e2function void selectTerminalNode(entity node)
 	self.player.SelectedNode = node
 end
+
+__e2setcost(10)
+e2function number entity:playtime()
+	if not (this and IsValid(this) and this:IsPlayer() and this.sa_data.playtime) then return -1 end
+	return this.sa_data.playtime
+end
+
+e2function number playtime()
+	return self.player.sa_data.playtime
+end
